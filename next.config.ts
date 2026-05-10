5da772150f061c5f2333671e04c921600c5dd4fa
Nandani2024-tech <nandani.24bcs10317@sst.scaler.com>
2026-05-10 21:53:43 +0530

    fix next.config to allow vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pdf-parse", "chromadb", "@huggingface/transformers", "onnxruntime-node"],
+  serverExternalPackages: ["pdf-parse", "chromadb", "@chroma-core/default-embed", "@huggingface/transformers", "onnxruntime-node"],
 };
 
 export default nextConfig;
